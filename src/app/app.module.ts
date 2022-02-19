2bfccabd05617e80309f1d45a96781cff84c22fb
Diana Coelho <coelhodiana04@gmail.com>
2022-02-19 11:32:23 -0400

    chore: remove bulma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import ptBr from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { registerLocaleData } from '@angular/common';
-import ptBr from '@angular/common/locales/pt';
+
 registerLocaleData(ptBr);
 
 @NgModule({
@@ -16,7 +16,6 @@ registerLocaleData(ptBr);
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FontAwesomeModule,
   ],
   providers: [FormBuilder, { provide: LOCALE_ID, useValue: 'pt-br' }],
   bootstrap: [AppComponent],
